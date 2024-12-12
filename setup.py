fcd74794817be2e2dc00fa55694e0e2cc4858a08
Mark Lehoczky <marklehoczky@gmail.com>
2024-12-12 23:30:50 +0100

    Add project information files

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="gherkin-processor",
+    version="0.0.1",
+    author="Mark Lehoczky",
+    author_email="mark.lehoczky@gmail.com",
+    description="",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://github.com/MarkLehoczky/gherkin-processor",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
