6a63f8e2f236cbf73e7e09b3a544ec20e4b10107
Mark Lehoczky <marklehoczky@gmail.com>
2025-03-22 02:14:41 +0100

    test7

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -12,41 +12,40 @@ echo "--------------------------------------------------"
 run_check() {
   local name=$1
   shift
-  echo "Running ${name}..."
   "$@" >> /dev/null 2>&1
   exit_status=$?
   if [ $exit_status -ne 0 ]; then
     echo "${name} failed with exit code ${exit_status}"
     overall_status=1
   else
-    echo "${name} passed."
+    echo "${name} passed"
   fi
 }
 
-# Code functionality check
-run_check "pytest" pytest --cov --cov-fail-under=90 tests/
+# Complete Test Suite
+run_check "Complete Test Suite" pytest --cov --cov-fail-under=90 tests/
 echo "------------------------------"
 
-# Code security check
-run_check "bandit" bandit --recursive gherkin_processor/
+# Project Security Scan
+run_check "Project Security Scan" bandit --recursive gherkin_processor/
 echo "------------------------------"
 
 # Code Linting check
-run_check "pylint" pylint --max-line-length=160 --fail-under=9.90 gherkin_processor/
+run_check "Linting Analysis" pylint --max-line-length=160 --fail-under=9.90 gherkin_processor/
 echo "------------------------------"
-run_check "mypy" mypy --strict gherkin_processor/
+run_check "Type Hint Analysis" mypy --strict gherkin_processor/
 echo "------------------------------"
 
 # Code Metrics check
-run_check "radon cc" radon cc gherkin_processor/
+run_check "Code Metrics Analysis (cyclomatic complexity)" radon cc gherkin_processor/
 echo "------------------------------"
-run_check "radon mi" radon mi gherkin_processor/
+run_check "Code Metrics Analysis (maintainability index)" radon mi gherkin_processor/
 echo "------------------------------"
 
 # Code Format check
-run_check "pycodestyle" pycodestyle --max-line-length=160 gherkin_processor/
+run_check "Code Style Check" pycodestyle --max-line-length=160 gherkin_processor/
 echo "------------------------------"
-run_check "pydocstyle" pydocstyle gherkin_processor/
+run_check "Documentation Style Check" pydocstyle gherkin_processor/
 
 # Final check for overall status
 if [ $overall_status -ne 0 ]; then
