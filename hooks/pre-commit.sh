d1f608c719999c7814d6911a598ebd341a631892
Mark Lehoczky <marklehoczky@gmail.com>
2025-03-22 02:17:02 +0100

    test8

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-# pre-commit git hook script
 
-# Initialize overall status to success.
 overall_status=0
 
 echo ""
 echo "=================================================="
 echo "           GIT PRE-COMMIT HOOK STARTED            "
 echo "--------------------------------------------------"
-# Function to run a command and record failure if it occurs.
+
 run_check() {
   local name=$1
   shift
@@ -22,32 +20,21 @@ run_check() {
   fi
 }
 
-# Complete Test Suite
 run_check "Complete Test Suite" pytest --cov --cov-fail-under=90 tests/
 echo "------------------------------"
 
-# Project Security Scan
 run_check "Project Security Scan" bandit --recursive gherkin_processor/
 echo "------------------------------"
 
-# Code Linting check
 run_check "Linting Analysis" pylint --max-line-length=160 --fail-under=9.90 gherkin_processor/
-echo "------------------------------"
 run_check "Type Hint Analysis" mypy --strict gherkin_processor/
-echo "------------------------------"
-
-# Code Metrics check
 run_check "Code Metrics Analysis (cyclomatic complexity)" radon cc gherkin_processor/
-echo "------------------------------"
 run_check "Code Metrics Analysis (maintainability index)" radon mi gherkin_processor/
 echo "------------------------------"
 
-# Code Format check
 run_check "Code Style Check" pycodestyle --max-line-length=160 gherkin_processor/
-echo "------------------------------"
 run_check "Documentation Style Check" pydocstyle gherkin_processor/
 
-# Final check for overall status
 if [ $overall_status -ne 0 ]; then
   echo "--------------------------------------------------"
   echo "       ONE OR MORE PRE-COMMIT HOOKS FAILED        "
