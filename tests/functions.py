17a7ab126f56ea0c146226d02977df5fb282b7c3
Mark Lehoczky <marklehoczky@gmail.com>
2025-04-02 09:52:37 +0200

    Fix optional file path issue

--- tests/functions.py
+++ tests/functions.py
@@ -3,8 +3,11 @@ from os.path import isfile, join
 
 
 def empty_output_directory():
-    output_dir = "tests/data/output"
-    for item in listdir(output_dir):
-        item_path = join(output_dir, item)
-        if isfile(item_path):
-            remove(item_path)
+    try:
+        output_dir = "tests/data/output/"
+        for item in listdir(output_dir):
+            item_path = join(output_dir, item)
+            if isfile(item_path):
+                remove(item_path)
+    except:
+        return
