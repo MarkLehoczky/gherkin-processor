36f3332d5793a42e317e155b2aec8f2a77e35217
Mark Lehoczky <marklehoczky@gmail.com>
2025-04-02 09:55:23 +0200

    Fix not tracked directory issue

--- tests/functions.py
+++ tests/functions.py
@@ -1,13 +1,12 @@
-from os import listdir, remove
-from os.path import isfile, join
+from os import listdir, makedirs, remove
+from os.path import exists, isfile, join
 
 
 def empty_output_directory():
-    try:
-        output_dir = "tests/data/output/"
-        for item in listdir(output_dir):
-            item_path = join(output_dir, item)
-            if isfile(item_path):
-                remove(item_path)
-    except:
-        return
+    output_dir = "tests/data/output/"
+    if not exists(output_dir):
+        makedirs(output_dir)
+    for item in listdir(output_dir):
+        item_path = join(output_dir, item)
+        if isfile(item_path):
+            remove(item_path)
